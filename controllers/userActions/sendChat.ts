ab12527e352fa5e8844d6933760985edb6ddd7e5
Lanre290 <lanre2967@gmail.com>
2026-02-22 06:57:47 +0100

    fix: fixed chat api mismatch

--- controllers/userActions/sendChat.ts
+++ controllers/userActions/sendChat.ts
@@ -52,7 +52,7 @@ export const sendChat = async (
     
 
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+      model: "gemini-2.5-flash-lite",
       contents: contents,
       config: {
         responseMimeType: "application/json",
